9ccb993a589e92f3f073fa9df1ae013d08ff560c
Heechul Jeong <76894779+Heezzul@users.noreply.github.com>
2021-01-04 13:51:03 +0900

    Add files via upload

---
+++ 1week/1week/main.cpp
@@ -0,0 +1,18 @@
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace cv; 
+
+int main() {
+
+	Mat imgColor = imread("C:/test.jpg", IMREAD_COLOR); //파일의 절대경로를 지정하고 색을 가지면서 imgColor에 할당 
+	Mat imgGray = imread("C:/test.jpg", IMREAD_GRAYSCALE); //이미지를 회색으로 imgGray에 할당 
+
+	imshow("color", imgColor); //imgColor을 color의 이름의 새로운 창으로 본다
+	imshow("gray", imgGray);//imgGray을 gray의 이름의 새로운 창으로 본다
+
+	
+	waitKey(5000);
+
+	return 0;
+}
